af25025ebf71563f48cf5d8249b74725a965b3b3
adrsh1022 <123776327+adrsh1022@users.noreply.github.com>
2025-12-03 18:53:29 +0530

    Update Program-1.java

--- Program-1.java
+++ Program-1.java
@@ -1,1 +1,43 @@
+import java.util.*;
+
+class Calculator
+{
+  public double calculator(double a, double b, String op) 
+  {
+    switch (op)
+    {
+       case "add":
+                return a + b;
+        case "sub":
+                return a - b;
+        case "mul":
+                return a * b;
+        case "div":
+                if (b == 0) 
+                {
+                    System.out.println("Error: Division by zero");
+                    return 0;
+                }
+                return a / b;
+         default:
+                System.out.println("Invalid operation");
+                return 0;
+    }
+  }
+}
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        String op = sc.next();
+
+        Calculator calc = new Calculator();
+        double result = calc.calculate(a, b, op);
+
+        System.out.println(result);
+    }
+}
 
