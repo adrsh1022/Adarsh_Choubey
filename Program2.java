ae8045c2e60fa4561703e292b6fce6bd2eb78b3c
adrsh1022 <123776327+adrsh1022@users.noreply.github.com>
2025-12-03 19:30:04 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Program2 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(); 
+        for (int i=0;i<a;i++) {
+            int odd=2*i+1;
+            System.out.print(odd);
+            if (i<a-1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
